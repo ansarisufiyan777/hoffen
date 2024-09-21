d983a1d822637d3a05961c3d9214e492fbd0fc86
sufiyangorgias <sufiyan.ansari@gorgias.com>
2024-09-21 16:28:43 +0200

    First commit

---
+++ Management-Team.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+
+
+<!-- Mirrored from spoilfieldsservices.in/Management-Team.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Mar 2023 07:14:49 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+<meta charset="utf-8">
+<title> Hoffen Group</title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="plugins/revolution/css/settings.css" rel="stylesheet" type="text/css"><!-- REVOLUTION SETTINGS STYLES -->
+<link href="plugins/revolution/css/layers.css" rel="stylesheet" type="text/css"><!-- REVOLUTION LAYERS STYLES -->
+<link href="plugins/revolution/css/navigation.css" rel="stylesheet" type="text/css"><!-- REVOLUTION NAVIGATION STYLES -->
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+
+<!--Color Switcher Mockup-->
+<link href="css/color-switcher-design.css" rel="stylesheet">
+
+<!--Color Themes-->
+<link id="theme-color-file" href="css/color-themes/default-theme.css" rel="stylesheet">
+
+
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/favicon.png" type="image/x-icon">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+</head>
+
+<body>
+
+<div class="page-wrapper">
+    
+    
+    <!-- Main Header-->
+    <header class="main-header header-style-one">
+    
+        <!--Header-Upper-->
+        <div class="header-upper">
+            <div class="auto-container clearfix">
+                
+                <div class="pull-left logo-outer">
+                    <div class="logo"><a href="index.html"><img
+                    width="292"
+                    height="92"
+                    src="images/logo.png"
+                    alt=""
+                    title=""
+                /></a></div>
+                </div>
+                    
+             
+                
+            </div>
+        </div>
+        <!--End Header Upper-->
+        
+        <!--Header Lower-->
+        <div class="header-lower">
+            <div class="auto-container">
+                <div class="nav-outer clearfix">
+                    <!-- Main Menu -->
+                    <nav class="main-menu">
+                        <div class="navbar-header">
+                            <!-- Toggle Button -->      
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li><a href="index.html">Home</a>
+                                   
+                                </li>
+
+                               
+
+  <li class="dropdown"><a href="Management-Team.php#">About </a>
+                                    <ul>
+                                     <li><a href="Corporatet.html">  Corporate</a></li>
+                                       <li><a href="Management-Team.php">Management Team</a></li>
+                                         <li><a href="Mission-Statement.html">Mission Statement</a></li> 
+                                     </ul>
+                                </li>
+                              
+
+
+                                
+                                <li><a href="Services.html">Supply & Services</a></li>
+                               
+                               
+                                
+                                
+
+                                <li><a href="contact.html">Contact </a></li>
+                             </ul>
+                        </div>
+                    </nav>
+                    <!-- Main Menu End-->
+                    
+                    <!--Search Box-->
+                   
+                    
+                   
+                </div>
+            </div>
+        </div>
+        <!--End Header Lower-->
+        
+    </header>    <!--End Main Header -->
+    
+    <!--Page Title-->
+    <section class="page-title" 
+    style="background-image:url(images/resource/s1.jpg)">
+    	<div class="auto-container">
+        	<h1>Management Team</h1>
+            <ul class="page-breadcrumb">
+            	<li><a href="index.html">Home</a></li>
+                <li>Management Team</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+    
+    <!--Workshop Section-->
+    <section class="workshop-section">
+    	<div class="auto-container">
+        	<div class="row ">
+            	<!--Content Column-->
+            	<div class="content-column col-md-9 col-sm-12 col-xs-12">
+                
+                </div>
+                <!--Image Column-->
+            
+            </div>
+
+            <div class="row">
+                <!--Content Column-->
+                <div class="content-column col-md-9 col-sm-12 col-xs-12">
+                    <div class="inner-column">
+                        <h2>TARSEM SINGH, DIRECTOR – OPS & SAFETY <br>
+(Ex. Director, OISD -Govt. Of India, Ministry of Petroleum & Natural Gas)
+
+</h2>
+                        <div class="text">+36 YEARS OF EXPERIENCE WORKED AT Various oilfields (Offshore & Onshore) Oil & Natural Gas Corporation Limited of India, Government of India and expertise in Rig operations and Safety. Last profile was Director (E&P), Oil Industry Safety Directorate, Ministry of Petroleum and Natural Gas Government of India and responsible for the audit and clearance of all the rigs (Onshore & Offshore) working in India.
+</div>
+                    </div>
+                </div>
+                <!--Image Column-->
+               
+            </div>
+            <div class="row">
+                <!--Content Column-->
+                <div class="content-column col-md-9 col-sm-12 col-xs-12">
+                    <div class="inner-column">
+                        <h2>PRAMOD KUMAR<br> (DIRECTOR MARKETING)
+</h2>
+                        <div class="text">21 years’ experience with International Oil & Gas Companies Employed by major drilling contractors including Jindal Drilling & Industries Ltd., Jagson International Limited, Foresight Limited, and has worked in most areas of the world & successfully developed business for their offshore jack up rigs. Joined Hoffen Group, as Marketing Director and responsible for marketing and business development. Recently Mr Kumar has worked for Jagson and deployed their 2 offshore jackup rigs with ONGC.
+</div>
+                    </div>
+                </div>
+                <!--Image Column-->
+              
+            </div>
+
+             <div class="row">
+                <!--Content Column-->
+                <div class="content-column col-md-9 col-sm-12 col-xs-12">
+                    <div class="inner-column">
+                        <h2>SUNIL ARORA <br>(DIRECTOR – FINANCE & SUPPLY MANAGEMENT)
+</h2>
+                        <div class="text">28 +years’ experience, specialized in strategic & corporate financial planning, supply chain management, accounting, budgeting, MIS & cost controls. The officer has in depth knowledge in audit, taxation, secretarial & legal compliances, corporate restructuring, M & A activities.
+</div>
+                    </div>
+                </div>
+              
+            </div>
+
+
+
+
+           
+            
+        </div>
+    </section>
+    <!--End Workshop Section-->
+    
+ 
+  
+    
+      
+    <!--Main Footer-->
+ <footer class="main-footer">
+        <div class="auto-container">
+            <!--Widgets Section-->
+            <div class="widgets-section">
+                <div class="row clearfix">
+                    
+                    <!--big column-->
+
+                            <!--Footer Column-->
+                            <div class="footer-column col-md-4 col-sm-6 col-xs-12">
+                                <div class="footer-widget logo-widget">
+                                    <h2>GET IN TOUCH</h2>
+
+         
+
+                                   
+         
+         <ul class="list-style-two text">
+             <li>  hello@thehoffengroup.com</li>
+             <li>  +919702892901/+918433863998</li>
+             <li>  Room No Nx-A2 35/56, Laxmi Oil Depot, AZAD NAGAR, <br />
+                        Ghatkopar West, Mumbal, Mumbai <br />
+                        Suburban, Maharashtra, 400084</li>
+             
+             
+         </ul>
+         
+         <div class="contact"><a href="contact.html"><img src="images/aa.png" alt="" title=""></a>
+         
+         <a class="btn-style-four" type="submit" name="submit-form">Contact Us</a>
+         </div>
+         
+
+                                </div>
+                            </div>
+                            
+                            <!--Footer Column-->
+                            <div class="footer-column col-md-4 col-sm-6 col-xs-12">
+                                <div class="footer-widget links-widget">
+                                    <h2>Useful Links</h2>
+                                    <div class="widget-content">
+                                        <ul class="list-style-two">
+                                            
+
+                              <li><a href="Corporatet.html">  About Us</a></li>
+                                <li><a href="Services.html">  Supply & Services</a></li>
+
+                               
+                              
+                              
+                                
+                                
+
+                                <li><a href="contact.html"> Contact </a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+                            
+
+                    <!--big column-->
+                    <div class="big-column col-md-4 col-sm-12 col-xs-12">
+                        <div class="row clearfix">
+                        
+                            <!--Footer Column-->
+                            <div class="footer-column col-md-12 col-sm-6 col-xs-12">
+                                <div class="footer-widget links-widget">
+                                    <h2>What We Do</h2>
+                                    <div class="widget-content">
+                                        <ul class="list-style-two">
+                                            <li>   Represent & Facilitate Clients with long term drilling contracts</li>
+
+                                             <li>  Jointly managing & operating Partners / Associates Assets.</li>
+                                              <li>  Support client with crew, equipments & spares and allied services for smooth execution of drilling contract.</li>
+                                               <li> Turnkey project for Rig refurbication & Reactivation</li>
+                                                <li> Turnkey projects for class renewal & Equipment Certifications (API, Cat IV, and Overhauling) for the drilling rig.</li>
+                                                <li>  Customized / Turnkey project to get the drilling rig third party Audit as per client’s requirement.</li>
+                                      
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                       
+                        
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        <!--Footer Bottom-->
+      
+        
+    </footer>
+        
+</div>
+<!--End pagewrapper-->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="icon fa fa-angle-double-up"></span></div>
+
+
+<script src="js/jquery.js"></script> 
+
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/appear.js"></script>
+<script src="js/script.js"></script>
+
+<!--Color Switcher-->
+</body>
+
+
+<!-- Mirrored from spoilfieldsservices.in/Management-Team.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 03 Mar 2023 07:14:49 GMT -->
+</html>
